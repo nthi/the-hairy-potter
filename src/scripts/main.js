7102a94635a2d53d39ace8473fb1d4bd47fdcc02
Natalie Thiele <natalie.a.a.thiele@gmail.com>
2022-06-24 14:12:09 -0400

    First push

---
+++ src/scripts/main.js
@@ -0,0 +1,23 @@
+// Imports go first
+import { makePottery } from './PotteryWheel.js'
+
+
+// Make 5 pieces of pottery at the wheel
+let mug = makePottery("mug", 5, 4)
+let plate = makePottery("plate", 3, 6)
+let stein = makePottery("stein", 10, 9)
+let vase = makePottery("vase", 25, 22)
+let bowl = makePottery("bowl", 20, 6)
+// console.log(mug)
+
+
+// Fire each piece of pottery in the kiln
+
+
+// Determine which ones should be sold, and their price
+
+
+// Invoke the component function that renders the HTML list
+
+
+
