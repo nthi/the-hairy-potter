dafebb15b130b08f3a7cd1f3e9741dfca0e4a10b
Natalie Thiele <natalie.a.a.thiele@gmail.com>
2022-06-24 15:33:03 -0400

    Add sell status array functions

---
+++ src/scripts/PotteryCatalog.js
@@ -0,0 +1,35 @@
+
+// Define a variable in the module with a value of an empty array. This array will store pottery that will be sold. Do not export this array.
+
+const potteryToSell = []
+
+// Define and export a function named toSellOrNotToSell that is responsible for determining if a piece of pottery should be sold.
+export const toSellOrNotToSell = (firedPotteryObject) => {
+    if (firedPotteryObject.weight >= 6) {
+        firedPotteryObject.price = 40
+    } else {
+        firedPotteryObject.price = 20
+    }
+
+    if (firedPotteryObject.cracked === false) {
+        potteryToSell.push(firedPotteryObject)
+    }
+    
+    return firedPotteryObject
+}
+
+export const usePottery = () => {
+    return potteryToSell.map(pot => ({...pot}))
+}
+
+// The toSellOrNotToSell function must accept a pottery object as input.
+
+// If the weight of the piece of pottery is greater than, or equal to, 6 then the function must add a price property with a value of 40.
+
+// If the weight of the piece of pottery is less than 6 then the function must add a price property with a value of 20.
+
+// If the pottery is not cracked, add the object to the module-level array of items to be sold.
+
+// Define and export a function named usePottery returns a copy of the array of items to be sold. Recall which array method creates a copy of the array.
+
+//from previous project: return database.fish.map(fish => ({...fish}))
--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,6 +1,8 @@
 // Imports go first
 import { makePottery } from './PotteryWheel.js'
 import { firePottery } from './Kiln.js'
+import { toSellOrNotToSell } from './PotteryCatalog.js'
+import { usePottery } from './PotteryCatalog.js'
 
 // Make 5 pieces of pottery at the wheel
 let mug = makePottery("mug", 5, 4)
@@ -8,26 +10,34 @@ let plate = makePottery("plate", 3, 6)
 let stein = makePottery("stein", 10, 9)
 let vase = makePottery("vase", 25, 22)
 let bowl = makePottery("bowl", 20, 6)
-// console.log(mug)
+//console.log(mug)
 
 
 // Fire each piece of pottery in the kiln
 let firedMug = firePottery(mug, 1500)
-console.log(firedMug)
+// console.log(firedMug)
 
 let firedPlate = firePottery(plate, 3000)
-console.log(firedPlate)
+// console.log(firedPlate)
 
 let firedStein = firePottery(stein, 1200)
-console.log(firedStein)
+// console.log(firedStein)
 
 let firedVase = firePottery(vase, 2100)
-console.log(firedVase)
+// console.log(firedVase)
 
 let firedBowl = firePottery(bowl, 1855)
-console.log(firedBowl)
+// console.log(firedBowl)
 
 // Determine which ones should be sold, and their price
+toSellOrNotToSell(firedMug)
+toSellOrNotToSell(firedPlate)
+toSellOrNotToSell(firedStein)
+toSellOrNotToSell(firedVase)
+toSellOrNotToSell(firedBowl)
+let sellList = usePottery()
+console.log(sellList)
+
 
 
 // Invoke the component function that renders the HTML list
