bbc562b82c76f1459153ec95ed16fa34a724ccbf
Natalie Thiele <natalie.a.a.thiele@gmail.com>
2022-06-24 14:39:05 -0400

    Add firing module

---
+++ src/scripts/Kiln.js
@@ -0,0 +1,35 @@
+export const firePottery = (potteryObject, setTemperature) => {
+    //The function must add a new property of fired with the value of true to the object.
+    potteryObject.fired = true
+
+    //The function must add a new property of cracked to the object.
+    potteryObject.cracked = ''
+
+    // If the temperature of the kiln is above 2200 degrees then cracked property must have a value of true.
+
+    // If the temperature of the kiln is at, or below, 2200 degrees then cracked property must have a value of false.
+    if (setTemperature > 2200) {
+        potteryObject.cracked = true
+    } else {
+        potteryObject.cracked = false
+    }
+
+    return potteryObject
+}
+
+/*Define and export a function named firePottery that is responsible for acting as a kiln.
+The function must accept the following values as input (i.e. it needs parameters), in the following order. If you don't remember, you can easily add new properties to objects in JavaScript.
+
+An object representing a piece of pottery that was made at the wheel in the makePottery function.
+
+A number specifying the firing temperature of the kiln.
+
+The function must add a new property of fired with the value of true to the object.
+
+The function must add a new property of cracked to the object.
+
+If the temperature of the kiln is above 2200 degrees then cracked property must have a value of true.
+
+If the temperature of the kiln is at, or below, 2200 degrees then cracked property must have a value of false.
+
+After both of the new properties have been added, return the augmented object.*/
--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,6 +1,6 @@
 // Imports go first
 import { makePottery } from './PotteryWheel.js'
-
+import { firePottery } from './Kiln.js'
 
 // Make 5 pieces of pottery at the wheel
 let mug = makePottery("mug", 5, 4)
@@ -12,7 +12,20 @@ let bowl = makePottery("bowl", 20, 6)
 
 
 // Fire each piece of pottery in the kiln
+let firedMug = firePottery(mug, 1500)
+console.log(firedMug)
 
+let firedPlate = firePottery(plate, 3000)
+console.log(firedPlate)
+
+let firedStein = firePottery(stein, 1200)
+console.log(firedStein)
+
+let firedVase = firePottery(vase, 2100)
+console.log(firedVase)
+
+let firedBowl = firePottery(bowl, 1855)
+console.log(firedBowl)
 
 // Determine which ones should be sold, and their price
 
